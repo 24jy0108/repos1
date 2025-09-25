f0fa13a53ea15fa395b51b5f9a05601fabab8ae0
24jy0108 <24jy0108@PC13.jynet2.jec.ac.jp>
2025-09-25 16:12:24 +0900

    mod for文で表示回数を10にする

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		for(int i = 0; i<5; i++) {
+		for(int i = 0; i<10; i++) {
 			System.out.println("HelloWorld");
 		}
 	}
