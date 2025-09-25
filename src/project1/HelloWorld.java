f1728febbd1958281442511d2c5ff9eb7372cdcf
24jy0108 <24jy0108@PC13.jynet2.jec.ac.jp>
2025-09-25 16:11:43 +0900

    for文で5回表示

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("HelloWorld");
-		System.out.println("HelloWorld");
+		for(int i = 0; i<5; i++) {
+			System.out.println("HelloWorld");
+		}
 	}
 }
